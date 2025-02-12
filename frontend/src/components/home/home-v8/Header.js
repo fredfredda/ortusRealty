ffa8b4541399713b148adfdf4ff9f02dba8d7832
fredfredda <136504513+fredfredda@users.noreply.github.com>
2025-02-12 15:32:48 +0300

    mobile menu testing

--- frontend/src/components/home/home-v8/Header.js
+++ frontend/src/components/home/home-v8/Header.js
@@ -45,12 +45,10 @@ const Header = () => {
   };
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("scroll", changeBackground);
-      return () => {
-        window.removeEventListener("scroll", changeBackground);
-      };
-    }
+    window.addEventListener("scroll", changeBackground);
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
   }, []);
 
   const handleLogout = async () => {
@@ -70,10 +68,8 @@ const Header = () => {
           );
         }
       } else if (data.success) {
-        if (typeof window !== "undefined" && window.localStorage) {
-          localStorage.removeItem("session");
-          localStorage.removeItem("token");
-        }
+        localStorage.removeItem("session");
+        localStorage.removeItem("token");
         deleteSession();
         toast.success("Logged out successfully");
         if (protectRoutes.includes(pathname)) {
@@ -88,124 +84,115 @@ const Header = () => {
   };
 
   if (isLoading === false)
-    return (
-      <>
-        <header
-          className={`header-nav nav-homepage-style light-header menu-home4 main-menu ${
-            navbar ? "sticky slideInDown animated" : ""
-          }`}
-        >
-          <nav className="posr">
-            <div className="container posr menu_bdrt1">
-              <div className="row align-items-center justify-content-between">
-                <div className="col-auto">
-                  <div className="d-flex align-items-center justify-content-between">
-                    <div className="logos mr40">
-                      <Link className="header-logo logo1" href="/">
-                        <Image
-                          layout="responsive"
-                          width={138}
-                          height={44}
-                          src="/images/ortus_realty_logo.png"
-                          alt="Header Logo"
-                        />
-                      </Link>
-                      <Link className="header-logo logo2" href="/">
-                        <Image
-                          layout="responsive"
-                          width={138}
-                          height={44}
-                          src="/images/ortus_realty_logo.png"
-                          alt="Header Logo"
-                        />
-                      </Link>
-                    </div>
-                    {/* End Logo */}
+  return (
+    <>
+      <header
+        className={`header-nav nav-homepage-style light-header menu-home4 main-menu ${
+          navbar ? "sticky slideInDown animated" : ""
+        }`}
+      >
+        <nav className="posr">
+          <div className="container posr menu_bdrt1">
+            <div className="row align-items-center justify-content-between">
+              <div className="col-auto">
+                <div className="d-flex align-items-center justify-content-between">
+                  <div className="logos mr40">
+                    <Link className="header-logo logo1" href="/">
+                      <Image
+                        width={138}
+                        height={44}
+                        src="/images/ortus_realty_logo.png"
+                        alt="Header Logo"
+                      />
+                    </Link>
+                    <Link className="header-logo logo2" href="/">
+                      <Image
+                        width={138}
+                        height={44}
+                        src="/images/ortus_realty_logo.png"
+                        alt="Header Logo"
+                      />
+                    </Link>
+                  </div>
+                  {/* End Logo */}
 
-                    <MainMenu />
-                    {/* End Main Menu */}
+                  <MainMenu />
+                  {/* End Main Menu */}
+                </div>
+              </div>
+              {/* End .col-auto */}
+
+              {!session?.userId ? (
+                <div className="col-auto">
+                  <div className="d-flex align-items-center">
+                    <a
+                      href={`/login?redirect=${pathname}`}
+                      className="login-info d-flex align-items-center"
+                      role="button"
+                    >
+                      <i className="far fa-user-circle fz16 me-2" />{" "}
+                      <span className="d-none d-xl-block">
+                        Login / Register
+                      </span>
+                    </a>
                   </div>
                 </div>
-                {/* End .col-auto */}
-
-                {!session?.userId ? (
-                  <div className="col-auto">
-                    <div className="d-flex align-items-center">
-                      <a
-                        href={`/login?redirect=${pathname}`}
-                        className="login-info d-flex align-items-center"
-                        role="button"
-                      >
-                        <i className="far fa-user-circle fz16 me-2" />{" "}
-                        <span className="d-none d-xl-block">
-                          Login / Register
-                        </span>
-                      </a>
-                    </div>
-                  </div>
-                ) : (
-                  <div className="col-6 col-lg-auto">
-                    <div className="text-center text-lg-end header_right_widgets">
-                      <ul className="mb0 d-flex justify-content-center justify-content-sm-end p-0">
-                        <li className=" user_setting">
-                          <div className="dropdown">
-                            <a
-                              className="btn"
-                              href="#"
-                              data-bs-toggle="dropdown"
-                            >
-                              <Image
-                                layout="responsive"
-                                width={44}
-                                height={44}
-                                src={`https://ui-avatars.com/api/?name=${
-                                  session?.lastName + "+" + session?.firstName
-                                }&background=random&rounded=true&size=44`}
-                                alt="user"
-                              />
-                              <i className="far fa-chevron-down ml5" />
-                            </a>
-                            <div className="dropdown-menu">
-                              <div className="user_setting_content">
-                                <div>
-                                  {menuItems.items.map((item, itemIndex) => (
-                                    <Link
-                                      key={itemIndex}
-                                      className={`dropdown-item ${
-                                        pathname == item.href
-                                          ? "-is-active"
-                                          : ""
-                                      } `}
-                                      href={item.href}
-                                    >
-                                      <i className={`${item.icon} mr10`} />
-                                      {item.text}
-                                    </Link>
-                                  ))}
-                                </div>
-                                <div>
-                                  <button
-                                    className="dropdown-item"
-                                    onClick={handleLogout}
+              ) : (
+                <div className="col-6 col-lg-auto">
+                  <div className="text-center text-lg-end header_right_widgets">
+                    <ul className="mb0 d-flex justify-content-center justify-content-sm-end p-0">
+                      <li className=" user_setting">
+                        <div className="dropdown">
+                          <a className="btn" href="#" data-bs-toggle="dropdown">
+                            <Image
+                              width={44}
+                              height={44}
+                              src={`https://ui-avatars.com/api/?name=${
+                                session?.lastName + "+" + session?.firstName
+                              }&background=random&rounded=true&size=44`}
+                              alt="user"
+                            />
+                            <i className="far fa-chevron-down ml5" />
+                          </a>
+                          <div className="dropdown-menu">
+                            <div className="user_setting_content">
+                              <div>
+                                {menuItems.items.map((item, itemIndex) => (
+                                  <Link
+                                    key={itemIndex}
+                                    className={`dropdown-item ${
+                                      pathname == item.href ? "-is-active" : ""
+                                    } `}
+                                    href={item.href}
                                   >
-                                    <i className="flaticon-exit mr10" />
-                                    Logout
-                                  </button>
-                                </div>
+                                    <i className={`${item.icon} mr10`} />
+                                    {item.text}
+                                  </Link>
+                                ))}
+                              </div>
+                              <div>
+                                <button
+                                  className="dropdown-item"
+                                  onClick={handleLogout}
+                                >
+                                  <i className="flaticon-exit mr10" />
+                                  Logout
+                                </button>
                               </div>
                             </div>
                           </div>
-                        </li>
-                      </ul>
-                    </div>
+                        </div>
+                      </li>
+                    </ul>
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
-          </nav>
-        </header>
-      </>
-    );
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Header;
